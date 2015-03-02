663e0035def3e8246ba89d56f6eccc4fdc022dd7
leeku11 <leeku11@Kwangyoung-Lees-MacBook-Pro.local>
2015-03-02 10:03:57 +0900

    [chg] optimize fuse bit

--- source/timymain/i2cReceiver/writeFW.bat
+++ source/timymain/i2cReceiver/writeFW.bat
@@ -1,3 +1,3 @@
-rem avrdude -c stk500v2 -P com3 -p attiny85 -U hfuse:w:0xDD:m -U lfuse:w:0x52:m
+avrdude -c stk500v2 -P com3 -p attiny85 -U hfuse:w:0xDD:m -U lfuse:w:0x42:m
 avrdude -c stk500v2 -P com3 -p attiny85 -U flash:w:main.hex:i
 pause;
--- source/timymain/i2cReceiver/writeFWusb.bat
+++ source/timymain/i2cReceiver/writeFWusb.bat
@@ -1,2 +1,3 @@
+avrdude -c avrispmkII -P USB -p attiny85 -U hfuse:w:0xDD:m -U lfuse:w:0x42:m
 avrdude -c avrispmkII -P USB -p attiny85 -U flash:w:main.hex:i
 pause;
